aac895cf4f9da6d08babbbb6169918aa2d3bf68b
kanishkmorey <86414024+kanishkmorey@users.noreply.github.com>
2022-03-02 14:14:45 +0530

    a

---
+++ assignments/practical1/w_11_8.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() 
+{
+
+  char line[150];
+  int vowels;
+
+  vowels = 0;
+
+  printf("Enter a line of string: ");
+  fgets(line, sizeof(line), stdin);
+
+  for (int i = 0; line[i] != '\0'; ++i) 
+  {
+    line[i] = tolower(line[i]);
+
+    if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' ||
+        line[i] == 'o' || line[i] == 'u') 
+        {
+            ++vowels;
+        }
+  }
+
+  printf("Vowels: %d\n\n", vowels);
+  return 0;
+}
